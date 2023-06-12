e02bbdbe496d1d3a03ef1bef530b4956652dc348
Artem Kulikov <chigar2010@yandex.ru>
2023-06-12 20:10:19 +0200

    add start_training

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from random import randint
 
-
 DEFAULT_ATTACK = 5
 DEFAULT_DEFENCE = 10
 DEFAULT_STAMINA = 80
@@ -62,6 +61,55 @@ class Healer(Character):
     RANGE_VALUE_DEFENCE = (2, 5)
     SPECIAL_BUFF = DEFAULT_DEFENCE + 30
     SPECIAL_SKILL = 'Защита'
+
+
+def start_training(character: Character) -> str:
+    """
+    Accepts an instance of the class as input.
+    Returns messages about the results of the character's training cycle.
+    """
+    comands: dict = {
+        'attack': character.attack,
+        'defence': character.defence,
+        'special': character.special,
+    }
+    print('Потренируйся управлять своими навыками.')
+    print(
+        'Введи одну из команд: attack — чтобы атаковать противника, '
+        'defence — чтобы блокировать атаку противника или '
+        'special — чтобы использовать свою суперсилу.'
+    )
+    print('Если не хочешь тренироваться, введи команду skip.')
+    cmd: str = None
+    while cmd != 'skip':
+        cmd = input('Введи команду: ')
+        if cmd in comands:
+            print(comands[cmd])
+    return 'Тренировка окончена.'
+
+
+def choice_char_class(char_name: str) -> Character:
+    """
+    Return instance of char class
+    """
+    game_classes: dict = {'warrior': Warrior, 'mage': Mage, 'healer': Healer}
+
+    approve_choice: str = None
+
+    while approve_choice != 'y':
+        selected_class = input(
+            'Введи название персонажа, '
+            'за которого хочешь играть: Воитель — warrior, '
+            'Маг — mage, Лекарь — healer: '
+        )
+        char_class: Character = game_classes[selected_class](char_name)
+        print(char_class)
+        approve_choice = input(
+            'Нажми (Y), чтобы подтвердить выбор, '
+            'или любую другую кнопку, '
+            'чтобы выбрать другого персонажа '
+        ).lower()
+    return char_class
 
 
 warrior = Warrior('Кодослав')
